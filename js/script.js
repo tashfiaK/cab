7f1f26ff86c7cb3f5b7182a4dee91e50b8659c67
Tashfia Khanom <k.tashfiastha@gmail.com>
2024-08-14 07:44:49 -0400

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -8,3 +8,20 @@ $(document).ready(function(){
         autoplaySpeed: 2000,
     });
   });
+  $(document).ready(function(){
+    $('.t_slider').slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+  });
+  $('.counter').counterUp();
+//   jQuery(document).ready(function($) {
+//         $('.counter').counterUp({
+//             delay: 10,
+//             time: 1500
+//         })
+//     })
